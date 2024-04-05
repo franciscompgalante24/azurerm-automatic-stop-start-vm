ac3d09cc48b35dbf5155e968aecb20506e32659e
Francisco Galante <120483444+franciscompgalante24@users.noreply.github.com>
2024-04-05 16:12:40 +0100

    Terraform scripts

---
+++ main.tf
@@ -0,0 +1,148 @@
+# ---------------------------------------------------------
+# MAIN
+# ---------------------------------------------------------
+
+data "azurerm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+
+data "azurerm_automation_account" "auto_acc" {
+  name                = var.automation_account_name
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+}
+
+# Check next saturday for stopping the virtual machine
+data "external" "next_saturday" {
+  program = ["python3", "${path.module}/python/next_saturday.py"]
+}
+
+# Check next sunday for starting the virtual machine
+data "external" "next_sunday" {
+  program = ["python3", "${path.module}/python/next_sunday.py"]
+}
+
+# Get variables of stop-start times to pass in azurerm_automation_schedule
+locals {
+  # Extract only the date from the full start_time string -> "YYYY-MM-DD"
+  start_date_stop_vm  = substr(data.external.next_saturday.result["start_time"], 0, 10)
+  start_date_start_vm = substr(data.external.next_sunday.result["start_time"], 0, 10)
+  # Append user-provided schedule hour to the start_date -> "YYYY-MM-DDT18:00:00+00:00")
+  custom_start_time_stop_vm  = format("%sT%02d:00:00+00:00", local.start_date_stop_vm, var.stop_vm_schedule.start_hour)
+  custom_start_time_start_vm = format("%sT%02d:00:00+00:00", local.start_date_start_vm, var.start_vm_schedule.start_hour)
+}
+
+# -----------------------------------------------------------------------------
+# Stop and Start Scripts into Automation Account
+# -----------------------------------------------------------------------------
+
+# Powershell script to Stop the VM
+data "local_file" "stop_vm" {
+  filename = "${path.module}/powershell/stop_vm.ps1"
+}
+
+# Powershell script to Start the VM
+data "local_file" "start_vm" {
+  filename = "${path.module}/powershell/start_vm.ps1"
+}
+
+# Assign Role to Automation Account for logging in to AZ-Powershell via Managed Identity
+# resource "azurerm_role_assignment" "automation_account_role" {
+#   scope                = data.azurerm_resource_group.resource_group.id
+#   role_definition_name = "Virtual Machine Contributor"
+#   principal_id         = data.azurerm_automation_account.auto_acc.identity[0].principal_id
+#   depends_on           = [data.azurerm_automation_account.auto_acc]
+# }
+
+# Resource Group Name in scope -> passed as input to Powershell Script
+resource "azurerm_automation_variable_string" "automation_account_rg_variable" {
+  name                    = "StopStartRGScope"
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  value                   = jsonencode(var.resource_group_name)
+  encrypted               = true
+
+  depends_on = [data.azurerm_automation_account.auto_acc]
+}
+
+# Create an Azure Automation Stop VM Runbook -> Powershell script content
+resource "azurerm_automation_runbook" "stop_vm_runbook" {
+  name                    = var.stop_vm_runbook_name
+  location                = data.azurerm_resource_group.resource_group.location
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Runbook for Stopping the Virtual Machine"
+  runbook_type            = "PowerShell"
+  content                 = data.local_file.stop_vm.content
+
+  depends_on = [data.local_file.stop_vm,
+  azurerm_automation_variable_string.automation_account_rg_variable]
+
+  tags = var.tags
+}
+
+# Create an Azure Automation Start VM Runbook -> Powershell script content
+resource "azurerm_automation_runbook" "start_vm_runbook" {
+  name                    = var.start_vm_runbook_name
+  location                = data.azurerm_resource_group.resource_group.location
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "Runbook for Starting the Virtual Machine"
+  runbook_type            = "PowerShell"
+  content                 = data.local_file.start_vm.content
+
+  depends_on = [data.local_file.start_vm,
+  azurerm_automation_variable_string.automation_account_rg_variable]
+
+  tags = var.tags
+}
+
+# Create an Automation Account Schedule
+resource "azurerm_automation_schedule" "stop_vm_schedule" {
+  name                    = var.stop_vm_schedule.name
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  description             = var.stop_vm_schedule.description
+  frequency               = var.stop_vm_schedule.frequency
+  interval                = var.stop_vm_schedule.interval
+  timezone                = var.stop_vm_schedule.timezone
+  week_days               = var.stop_vm_schedule.week_days
+  start_time              = local.custom_start_time_stop_vm
+}
+
+resource "azurerm_automation_schedule" "start_vm_schedule" {
+  name                    = var.start_vm_schedule.name
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  description             = var.start_vm_schedule.description
+  frequency               = var.start_vm_schedule.frequency
+  interval                = var.start_vm_schedule.interval
+  timezone                = var.start_vm_schedule.timezone
+  week_days               = var.start_vm_schedule.week_days
+  start_time              = local.custom_start_time_start_vm
+}
+
+# Associate Automation Account Schedule to Runbook -> Powershell script to Stop VM During Weekends
+resource "azurerm_automation_job_schedule" "automation_schedule_stop_vm" {
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  schedule_name           = azurerm_automation_schedule.stop_vm_schedule.name
+  runbook_name            = azurerm_automation_runbook.stop_vm_runbook.name
+
+  depends_on = [azurerm_automation_runbook.stop_vm_runbook,
+  azurerm_automation_schedule.stop_vm_schedule]
+}
+
+# Associate Automation Account Schedule to Runbook -> Powershell script to Start VM During Weekends
+resource "azurerm_automation_job_schedule" "automation_schedule_start_vm" {
+  resource_group_name     = data.azurerm_resource_group.resource_group.name
+  automation_account_name = data.azurerm_automation_account.auto_acc.name
+  schedule_name           = azurerm_automation_schedule.start_vm_schedule.name
+  runbook_name            = azurerm_automation_runbook.start_vm_runbook.name
+
+  depends_on = [azurerm_automation_runbook.start_vm_runbook,
+  azurerm_automation_schedule.start_vm_schedule]
+}
---
+++ variables.tf
@@ -0,0 +1,60 @@
+# ---------------------------------------------------------
+# VARIABLES
+# ---------------------------------------------------------
+
+variable "tags" {
+  description = "Tags for the Automatic Stop-Start VM"
+  type        = map(string)
+  default     = {}
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Location of the Resource Group"
+  type        = string
+}
+
+variable "automation_account_name" {
+  description = "Name of the Automation Account"
+  type        = string
+}
+
+variable "stop_vm_runbook_name" {
+  description = "Name of the Automation Runbook that stops the vm"
+  type        = string
+}
+
+variable "start_vm_runbook_name" {
+  description = "Name of the Automation Runbook that starts the vm"
+  type        = string
+}
+
+variable "stop_vm_schedule" {
+  description = "Details about the Stop VM Schedule"
+  type = object({
+    name        = string
+    description = string
+    frequency   = string
+    interval    = number
+    timezone    = string
+    week_days   = list(string)
+    start_hour  = number
+  })
+}
+
+variable "start_vm_schedule" {
+  description = "Details about the Start VM Schedule"
+  type = object({
+    name        = string
+    description = string
+    frequency   = string
+    interval    = number
+    timezone    = string
+    week_days   = list(string)
+    start_hour  = number
+  })
+}
